59bd1185d6972c4d17e340d6dade2de6529b441c
unknown <ljinwoo9633@gmail.com>
2020-06-01 18:37:44 +0900

    'edit'

--- day7/help/index.js
+++ day7/help/index.js
@@ -1,5 +1,9 @@
 let fs = require('fs');
 
+
+// txt파일을 작성하는데 사용하는 함수입니다!
+// fileName에 '파일명'을 적고 data에 적고싶은 데이터를 적어주세요!
+// ex) WriteTextFile("Helloworld", "Hello!") => Helloworld이름을 가지고 데이터 Hello!를 가지는 txt파일을 만들어준다.
 let WriteTextFile = async (fileName, data) => {
     try
     {
@@ -23,6 +27,10 @@ let WriteTextFile = async (fileName, data) => {
     }
 }
 
+
+//txt 파일을 읽는 함수입니다
+//fileName에 txt파일명(.txt 적지마시고!)을 적으면 읽어요!
+//ex) ReadTextFile("Helloworld") => Helloworld.txt에 있는 데이터를 읽는다.
 let ReadTextFile = async (fileName) => {
     try
     {
